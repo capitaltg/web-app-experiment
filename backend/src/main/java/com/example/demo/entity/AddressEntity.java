4c2d66e4befdaf631d5052312ef8408cbcc7e885
jwdomes <jwdomes@gmail.com>
2025-05-13 12:56:31 -0400

    Update employee entity with address and salary

---
+++ backend/src/main/java/com/example/demo/entity/AddressEntity.java
@@ -0,0 +1,50 @@
+package com.example.demo.entity;
+
+import java.time.ZonedDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Entity
+@Builder
+@Table(name = "address")
+public class AddressEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotBlank(message = "Field 'streetAddress1' is required.")
+  @Column(name = "street_address_1")
+  private String streetAddress1;
+
+  @Column(name = "street_address_2")
+  private String streetAddress2;
+  
+  @NotBlank(message = "Field 'city' is required.")
+  @Column(name = "city")
+  private String city;
+
+  @NotBlank(message = "Field 'state' is required.")
+  @Column(name = "state")
+  private String state;
+
+  @NotBlank(message = "Field 'zipCode' is required.")
+  @Column(name = "zip")
+  private String zipCode;
+
+  @Column(name = "zip_ext")
+  private String zipCodeExtension;
+
+  @CreationTimestamp private ZonedDateTime createdAt;
+}
--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,16 +1,29 @@
 CREATE TABLE department (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL UNIQUE,
-    description VARCHAR(255),
-    created_at TIMESTAMP NOT NULL
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  description VARCHAR(255),
+  created_at TIMESTAMP NOT NULL
+);
+
+CREATE TABLE address (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  street_address_1 VARCHAR(255) NOT NULL,
+  street_address_2 VARCHAR(255),
+  city VARCHAR(255) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  zip VARCHAR(5) NOT NULL,
+  zip_ext VARCHAR(4)
 );
 
 CREATE TABLE employee (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255),
-    last_name VARCHAR(255) NOT NULL,
-    department_id BIGINT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  middle_name VARCHAR(255),
+  last_name VARCHAR(255) NOT NULL,
+  salary INTEGER NOT NULL,
+  address_id BIGINT,
+  department_id BIGINT NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  FOREIGN KEY (address_id) REFERENCES address(id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
